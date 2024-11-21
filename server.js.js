4dfa00601effe957b69eaaeca7c100cfc1b061b5
weiguya <weiguya@gmail.com>
2024-11-21 21:23:41 +0700

    Update server.js.js

--- server.js.js
+++ server.js.js
@@ -7,15 +7,16 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
+// ตั้งค่า static files
 app.use(express.static(path.join(__dirname, 'public')));
 
+// การตั้งค่า Socket.IO
 io.on('connection', (socket) => {
   console.log('A user connected');
 
-  // เมื่อส่งข้อความในห้องแชท
   socket.on('chat message', (data) => {
-    console.log('Message from ' + data.username + ": " + data.message);  // Log the message in the server
-    io.emit('chat message', data);  // ส่งข้อความไปให้ทุกคน (พร้อมชื่อและข้อความ)
+    console.log('Message from ' + data.username + ": " + data.message);
+    io.emit('chat message', data);
   });
 
   socket.on('disconnect', () => {
@@ -23,6 +24,7 @@ io.on('connection', (socket) => {
   });
 });
 
+// เริ่มเซิร์ฟเวอร์
 server.listen(3000, () => {
   console.log('Server is running on http://localhost:3000');
 });
